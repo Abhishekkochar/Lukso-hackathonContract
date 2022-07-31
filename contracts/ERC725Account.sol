4a46e24c8e7bd178dbc039995fa1066871a6f2b2
Sol-Devloper <blockhunt14@gmail.com>
2022-07-31 20:04:30 +1200

    UP file - Key issue

--- contracts/ERC725Account.sol
+++ contracts/ERC725Account.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.0;
 
-import "@lukso/lsp-smart-contracts/contracts/LSP0ERC725Account/LSP0ERC725Account.sol";
+import {LSP0ERC725Account} from "@lukso/lsp-smart-contracts/contracts/LSP0ERC725Account/LSP0ERC725Account.sol";
+import {LSP1_UniversalProfile} from "./contracts/LSP1_UniversalProfile.sol";
 
-prag
-+ma solidity ^0.8.0;
+contract UniversalProfile is LSP0ERC725Account, LSP1_UniversalProfile {}
